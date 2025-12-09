e2995bb37bef184eb264980c91d10f0b14118c00
Nikhil Singhal <nikhilagarwal6305@gmail.com>
2025-12-09 16:08:06 +0530

    Todo list

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
-    var allElems = document.querySelectorAll('.elem');
-    var allfullElem = document.querySelectorAll('.fullElem');
-    var allfullElemBack = document.querySelectorAll('.fullElem .back');
-    
-    allElems.forEach(function(elem){
-        elem.addEventListener('click', function(){
-            allfullElem[elem.id].style.display= 'block';
-        })
-    })
-    
-    allfullElemBack.forEach(function(back){
-        back.addEventListener('click', function(){
-            allfullElem[back.id].style.display= 'none';    
-        })
+function openFeatures() {
+  var allElems = document.querySelectorAll(".elem");
+  var allfullElem = document.querySelectorAll(".fullElem");
+  var allfullElemBack = document.querySelectorAll(".fullElem .back");
+
+  allElems.forEach(function (elem) {
+    elem.addEventListener("click", function () {
+      allfullElem[elem.id].style.display = "block";
     });
+  });
 
-function renderTask(){
+  allfullElemBack.forEach(function (back) {
+    back.addEventListener("click", function () {
+      allfullElem[back.id].style.display = "none";
+    });
+  });
+}
+openFeatures();
+    
+function todoList() {
+  let currentTask = [];
 
-    let sum =''
-    currentTask.forEach(function(elem, idx){
-        sum += `
+  if (localStorage.getItem("currentTask")) {
+    currentTask = JSON.parse(localStorage.getItem("currentTask"));
+  } else {
+    console.log("Task List is empty");
+  }
+  function renderTask() {
+    var alltask = document.querySelector(".allTask");
+    let sum = "";
+    currentTask.forEach(function (elem, idx) {
+      sum += `
         <div class="task">
         <div>
         <h2>${elem.task}</h2>
@@ -26,60 +37,42 @@ function renderTask(){
         </div>
         <button class="complete" id="${idx}"> Mark as Complete</button>
         </div>
-        `
-        localStorage.setItem('currentTask', JSON.stringify(currentTask))
-})
+        `;
+    });
 
-alltask.innerHTML=sum;
+    alltask.innerHTML = sum;
+    localStorage.setItem("currentTask", JSON.stringify(currentTask));
+
+    document.querySelectorAll(".task button").forEach(function (btn) {
+      btn.addEventListener("click", function () {
+        currentTask.splice(btn.id, 1);
+        renderTask();
+
+        if (currentTask.length == 0) {
+          localStorage.clear();
+        }
+      });
+    });
+  }
+
+  renderTask();
+  let form = document.querySelector(".addTask form");
+  let taskinput = document.querySelector(".addTask form input");
+  let taskDetailsinput = document.querySelector(".addTask form textarea");
+  let checkImpo = document.querySelector("#checkbox");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    currentTask.push({
+      task: taskinput.value,
+      details: taskDetailsinput.value,
+      impo: checkImpo.checked,
+    });
+    taskDetailsinput.value = "";
+    taskinput.value = "";
+    checkImpo.checked = false;
+
+    renderTask();
+  });
 }
-
-let form = document.querySelector('.addTask form')
-let taskinput = document.querySelector('.addTask form input')
-let taskDetailsinput = document.querySelector('.addTask form textarea')
-let checkImpo = document.querySelector('#checkbox')
-let alltask = document.querySelector('.allTask')
-
-let currentTask = []
-
-if(localStorage.getItem('currentTask')){
-    currentTask = JSON.parse(localStorage.getItem('currentTask'))
-}else{
-    console.log('Task List is empty');
-    
-}
-renderTask()
-
-
-form.addEventListener('submit', function(e){
-    e.preventDefault()
-    currentTask.push(
-        {
-            task:taskinput.value, details:
-            taskDetailsinput.value,
-            impo:checkImpo.checked
-        });
-    taskDetailsinput.value=''
-    taskinput.value=''
-    checkImpo.checked=false
-
-    renderTask()
-    location.reload()
-    
-})
-
-let markComplete = document.querySelectorAll('.task button')
-
-
-markComplete.forEach(function(btn){
-    btn.addEventListener('click', function(){
-                currentTask.splice(btn.id,1);
-                renderTask()
-                location.reload()
-
-                if(currentTask.length==0){
-                    localStorage.clear();
-                }
-    })
-})
-
-
+todoList();
